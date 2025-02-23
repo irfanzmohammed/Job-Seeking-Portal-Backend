7f76d0b2b375ad772aac4eeda62af2b5ed4b8299
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-02-23 17:17:20 +0530

    ok code

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { login, register, logout, getUser, getUserSkills, addUserSkill } from "../controllers/userController.js";
-
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -11,16 +11,17 @@ router.post("/register", register);
 router.post("/login", login);
 
 // Route for user logout (requires authentication)
-router.get("/logout",  logout);
+router.get("/logout", isAuthenticated, logout);
 
 // Route for getting logged-in user details (requires authentication)
-router.get("/getuser", getUser);
+router.get("/getuser", isAuthenticated, getUser);
 
 // Route for fetching job seeker skills (requires authentication)
-router.get("/skills",  getUserSkills);
+router.get("/skills", isAuthenticated, getUserSkills);
 
 // Route for adding a new skill to user's profile (requires authentication)
-router.post("/skills/add", addUserSkill);
+router.post("/skills/add", isAuthenticated, addUserSkill);
+
 
 export default router;
 
